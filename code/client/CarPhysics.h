37d2fca8b965f42c0c3bb1ab870010a52c71380d
Beau McCartney <beau@beaumccartney.xyz>
2023-01-12 12:49:31 -0700

    define asset filepath based on operating system

--- code/client/CarPhysics.h
+++ code/client/CarPhysics.h
@@ -2,7 +2,14 @@
 #include "toml++/toml.h"
 
 #define FILEPATH "configs/CarPhysics.toml"
+
+// assets filepath based on which os
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+#define ASSETS_FILEPATH
+"../../../../assets/configs/CarPhysics.toml"
+#else
 #define ASSETS_FILEPATH "../../code/assets/configs/CarPhysics.toml"
+#endif
 
 struct CarPhysics {
 	float m_suspension_force;
