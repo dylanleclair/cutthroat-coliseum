3e927449a5f23bd587bef582b2edb2a2d9e34595
Beau McCartney <beau@beaumccartney.xyz>
2023-01-20 17:50:36 -0700

    define _DEBUG only when not on windows
    see note in the comments this commit added

--- code/client/Physx.h
+++ code/client/Physx.h
@@ -1,4 +1,10 @@
+// HACK(beau): VS is defining this for us and complaining
+// but without vs its not defined. Solin is to find a way
+// to get cmake to define this regardless I think
+// - Beau
+#if !defined(WIN32) && !defined(_WIN32) && !defined(__WIN32)
 #define _DEBUG
+#endif
 #include "PxPhysicsAPI.h"
 
 extern physx::PxDefaultAllocator      gAllocator;
