16b260679f80022a4f1f119688cdfd2e484b6675
staytheknight <staytheknight@gmail.com>
2023-02-17 14:33:50 -0700

    Added Rigid Body Params

--- code/client/ImGuiDebug.cpp
+++ code/client/ImGuiDebug.cpp
@@ -1,17 +1,51 @@
 #include "ImGuiDebug.h"
 #include <iostream>
 
+//if (ImGui::TreeNode("Bullets"))
+//{
+//	ImGui::BulletText("Bullet point 1");
+//	ImGui::BulletText("Bullet point 2\nOn multiple lines");
+//	if (ImGui::TreeNode("Tree node"))
+//	{
+//		ImGui::BulletText("Another bullet point");
+//		ImGui::TreePop();
+//	}
+//	ImGui::Bullet(); ImGui::Text("Bullet point 3 (two calls)");
+//	ImGui::Bullet(); ImGui::SmallButton("Button");
+//	ImGui::TreePop();
+//}
+
 // Initializes variables
 void variableInit() {
 	rigid_mass = gVehicle.mBaseParams.rigidBodyParams.mass;
+	rigid_MOI = gVehicle.mBaseParams.rigidBodyParams.moi;
 }
 
 void vehicleTuning() {
 	ImGui::Begin("Tuning");
 
-	if (ImGui::InputFloat("Mass", &rigid_mass)) {
-		gVehicle.mBaseParams.rigidBodyParams.mass = rigid_mass;
+	if (ImGui::TreeNode("Rigid Body:")) {
+
+		if (ImGui::InputFloat("Mass", &rigid_mass)) {
+			gVehicle.mBaseParams.rigidBodyParams.mass = rigid_mass;
+		}
+
+		if (ImGui::TreeNode("MOI:")) {
+			if (ImGui::InputFloat("MOI x: ", &rigid_MOI.x)) {
+				gVehicle.mBaseParams.rigidBodyParams.moi.x = rigid_MOI.x;
+			}
+			if (ImGui::InputFloat("MOI y: ", &rigid_MOI.y)) {
+				gVehicle.mBaseParams.rigidBodyParams.moi.y = rigid_MOI.y;
+			}
+			if (ImGui::InputFloat("MOI z: ", &rigid_MOI.z)) {
+				gVehicle.mBaseParams.rigidBodyParams.moi.z = rigid_MOI.z;
+			}
+			ImGui::TreePop();
+		}
+		ImGui::TreePop();
 	}
+
+
 
 	ImGui::End();
 }
--- code/client/ImGuiDebug.h
+++ code/client/ImGuiDebug.h
@@ -3,7 +3,12 @@
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
 
+#include <glm/glm.hpp>
+
+using namespace glm;
+
 static float rigid_mass;
+static physx::PxVec3 rigid_MOI;
 
 void variableInit();
 void vehicleTuning();
