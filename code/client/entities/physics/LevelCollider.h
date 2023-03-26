cf9173d4bbc3a00f3ac77bef85cd0174f9a563a8
dylanleclair <dylan.leclair@icloud.com>
2023-03-26 15:56:42 -0600

    some fixes to level geom

--- code/client/entities/physics/LevelCollider.h
+++ code/client/entities/physics/LevelCollider.h
@@ -1,37 +1,6 @@
 #pragma once
-#include "PxPhysicsAPI.h"
-#include <vector>
-#include <iostream>
-#include "../../systems/GraphicsSystem.h"
-#include <string>
-#include "../../systems/PhysicsSystem.h"
+#include "MeshCollider.h"
 
 using namespace physics;
 
-struct LevelCollider
-{ 
-    physx::PxTriangleMesh * LevelCollider::cookLevel(glm::mat4 transform); 
-    void LevelCollider::initLevelRigidBody(physx::PxTriangleMesh* levelMesh);
-
-
-    // physx::PxTriangleMesh * cookLevel(std::vector<physx::PxVec3> levelVertices, std::vector<physx::PxU32> levelIndices, physx::PxPhysics* gPhysics, physx::PxCooking *gCooking);
-    // void LevelCollider::initLevelRigidBody(physx::PxPhysics* gPhysics, physx::PxScene* scene, physx::PxMaterial* material, physx::PxTriangleMesh* levelMesh);
-
-    void Initialize(CPU_Geometry& levelGeom, PhysicsSystem& physics);
-    void Initialize(std::string modelName, PhysicsSystem& physics);
-
-    void LevelCollider::release();
-    void LevelCollider::initLevelRigidBody(physx::PxTriangleMesh* levelMesh, physx::PxMaterial* material);
-    PxRigidActor* getActor() {return m_level; };
-    PxShape* getShape() {return m_levelShape; };
-
-
-private:
-    std::vector<physx::PxVec3> levelVertices = std::vector<physx::PxVec3>();
-    std::vector<physx::PxU32> levelIndices = std::vector<physx::PxU32>();
-    PhysicsSystem* m_physicsSystem;
-
-    physx::PxShape* m_levelShape;
-    physx::PxRigidStatic* m_level;
-
-};
+struct LevelCollider : MeshCollider {};
---
+++ code/client/entities/physics/MeshCollider.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "PxPhysicsAPI.h"
+#include <vector>
+#include <iostream>
+#include "../../systems/GraphicsSystem.h"
+#include <string>
+#include "../../systems/PhysicsSystem.h"
+
+using namespace physics;
+
+struct MeshCollider
+{
+  physx::PxTriangleMesh *MeshCollider::cookLevel(glm::mat4 transform);
+  void MeshCollider::initLevelRigidBody(physx::PxTriangleMesh *levelMesh);
+
+  // physx::PxTriangleMesh * cookLevel(std::vector<physx::PxVec3> levelVertices, std::vector<physx::PxU32> levelIndices, physx::PxPhysics* gPhysics, physx::PxCooking *gCooking);
+  // void MeshCollider::initLevelRigidBody(physx::PxPhysics* gPhysics, physx::PxScene* scene, physx::PxMaterial* material, physx::PxTriangleMesh* levelMesh);
+
+  void Initialize(CPU_Geometry &levelGeom, PhysicsSystem &physics);
+  void Initialize(std::string modelName, PhysicsSystem &physics);
+
+  void MeshCollider::release();
+  void MeshCollider::initLevelRigidBody(physx::PxTriangleMesh *levelMesh, physx::PxMaterial *material);
+  PxRigidActor *getActor() { return m_level; };
+  PxShape *getShape() { return m_levelShape; };
+
+private:
+  std::vector<physx::PxVec3> levelVertices = std::vector<physx::PxVec3>();
+  std::vector<physx::PxU32> levelIndices = std::vector<physx::PxU32>();
+  PhysicsSystem *m_physicsSystem;
+
+  physx::PxShape *m_levelShape;
+  physx::PxRigidStatic *m_level;
+};
