95a8a53989d69a816dcece81cc83b2e62e73ced9
dylanleclair <dylan.leclair@icloud.com>
2023-03-26 17:59:53 -0600

    load pillars, make them collide!

--- code/client/client.cpp
+++ code/client/client.cpp
@@ -309,23 +309,50 @@ int main(int argc, char* argv[]) {
 	CPU_Geometry obstacle_geom = CPU_Geometry();
 	GraphicsSystem::importOBJ(obstacle_geom, "obstacles-mesh.obj");
 
-	// give obstacles a collider
-	Guid obstacle_collider_e = mainScene.CreateEntity().guid;
-	mainScene.AddComponent(obstacle_collider_e, ObstacleCollider());
-	ObstacleCollider& new_obstacle_collider = mainScene.GetComponent<ObstacleCollider>(obstacle_collider_e);
-	new_obstacle_collider.Initialize(obstacle_geom, physicsSystem);
-	physx::PxTriangleMesh* new_obstacle_collider_mesh = new_obstacle_collider.cookLevel(glm::scale(glm::mat4(1), glm::vec3(1.0)));
-	new_obstacle_collider.initLevelRigidBody(new_obstacle_collider_mesh, lMaterial);
 
-	// we'll have to programmatically read the obstacles file ._.
+	std::vector<Guid> obstacles;
+	for (int i = 1; i <= 13; i++)
+	{
+		char buffer[50];
 
-	// render the obstacles
+		sprintf(buffer, "obstacles/Pillar_%03d.obj", i);
+		// load in obstacle collider & send it for rendering
 
-	TransformComponent obs_t = TransformComponent();
-	RenderModel obs_r = RenderModel();
-	GraphicsSystem::importOBJ(obs_r,"obstacles-mesh.obj");
-	mainScene.AddComponent(obstacle_collider_e, obs_t);
-	mainScene.AddComponent(obstacle_collider_e, obs_r);
+		CPU_Geometry obstacle_geom = CPU_Geometry();
+		GraphicsSystem::importOBJ(obstacle_geom, buffer);
+
+		Guid obstacle_collider_e = mainScene.CreateEntity().guid;
+		mainScene.AddComponent(obstacle_collider_e, ObstacleCollider());
+		ObstacleCollider& new_obstacle_collider = mainScene.GetComponent<ObstacleCollider>(obstacle_collider_e);
+		new_obstacle_collider.Initialize(obstacle_geom, physicsSystem);
+		physx::PxTriangleMesh* new_obstacle_collider_mesh = new_obstacle_collider.cookLevel(glm::scale(glm::mat4(1), glm::vec3(1.0)));
+		new_obstacle_collider.initLevelRigidBody(new_obstacle_collider_mesh, lMaterial);
+	
+	
+		TransformComponent obs_t = TransformComponent();
+		RenderModel obs_r = RenderModel();
+		GraphicsSystem::importOBJ(obs_r,buffer);
+		mainScene.AddComponent(obstacle_collider_e, obs_t);
+		mainScene.AddComponent(obstacle_collider_e, obs_r);
+	}
+
+	// // give obstacles a collider
+	// Guid obstacle_collider_e = mainScene.CreateEntity().guid;
+	// mainScene.AddComponent(obstacle_collider_e, ObstacleCollider());
+	// ObstacleCollider& new_obstacle_collider = mainScene.GetComponent<ObstacleCollider>(obstacle_collider_e);
+	// new_obstacle_collider.Initialize(obstacle_geom, physicsSystem);
+	// physx::PxTriangleMesh* new_obstacle_collider_mesh = new_obstacle_collider.cookLevel(glm::scale(glm::mat4(1), glm::vec3(1.0)));
+	// new_obstacle_collider.initLevelRigidBody(new_obstacle_collider_mesh, lMaterial);
+
+	// // we'll have to programmatically read the obstacles file ._.
+
+	// // render the obstacles
+
+	// TransformComponent obs_t = TransformComponent();
+	// RenderModel obs_r = RenderModel();
+	// GraphicsSystem::importOBJ(obs_r,"obstacles-mesh.obj");
+	// mainScene.AddComponent(obstacle_collider_e, obs_t);
+	// mainScene.AddComponent(obstacle_collider_e, obs_r);
 
 	ecs::Entity road_e = mainScene.CreateEntity();
 	TransformComponent road_t = TransformComponent();
