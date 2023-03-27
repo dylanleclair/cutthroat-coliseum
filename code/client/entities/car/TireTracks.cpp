34254aa8ebd02bed52a5b78cd6611d3ab6803a2d
staytheknight <staytheknight@gmail.com>
2023-03-26 22:04:40 -0600

    Made tire tracks only show up on skidding

--- code/client/entities/car/TireTracks.cpp
+++ code/client/entities/car/TireTracks.cpp
@@ -118,7 +118,10 @@ void updateCarVFX(ecs::Scene mainScene, float dt) {
 		TransformComponent& rightTireTransform = mainScene.GetComponent<TransformComponent>(vfx.trackGuids[1]);
 		TransformComponent& leftTireTransform = mainScene.GetComponent<TransformComponent>(vfx.trackGuids[2]);
 
-		if (car->m_Vehicle.mBaseState.roadGeomStates[0].hitState && car->m_Vehicle.mBaseState.roadGeomStates[1].hitState) {
+		float maxSlip = 0.6f;
+		
+		if (car->m_Vehicle.mBaseState.roadGeomStates[0].hitState && car->m_Vehicle.mBaseState.roadGeomStates[1].hitState
+			&& (car->m_Vehicle.mBaseState.tireSlipStates->slips[1] > maxSlip || car->m_Vehicle.mBaseState.tireSlipStates->slips[1] < -maxSlip)) {
 			previousStates.at(i).at(0) = true;
 			glm::vec3 frontTirePosition = frontTireTransform.getTransformationMatrix() * glm::vec4(0, y_offset, 0, 1);
 			if (glm::length(frontTirePosition - frontTireTracks.g_previousPosition()) > 1) {
@@ -135,7 +138,7 @@ void updateCarVFX(ecs::Scene mainScene, float dt) {
 		}
 
 		//right tire
-		if (car->m_Vehicle.mBaseState.roadGeomStates[2].hitState) {
+		if (car->m_Vehicle.mBaseState.roadGeomStates[2].hitState && (car->m_Vehicle.mBaseState.tireSlipStates->slips[1] > maxSlip || car->m_Vehicle.mBaseState.tireSlipStates->slips[1] < -maxSlip)) {
 			previousStates.at(i).at(1) = true;
 			glm::vec3 rightTirePosition = rightTireTransform.getTransformationMatrix() * glm::vec4(0, y_offset, 0, 1);
 			if (glm::length(rightTirePosition - rightTireTracks.g_previousPosition()) > 1) {
@@ -152,7 +155,7 @@ void updateCarVFX(ecs::Scene mainScene, float dt) {
 		}
 
 		//left tire
-		if (car->m_Vehicle.mBaseState.roadGeomStates[3].hitState) {
+		if (car->m_Vehicle.mBaseState.roadGeomStates[3].hitState && (car->m_Vehicle.mBaseState.tireSlipStates->slips[1] > maxSlip || car->m_Vehicle.mBaseState.tireSlipStates->slips[1] < -maxSlip)) {
 			previousStates.at(i).at(2) = true;
 			glm::vec3 leftTirePosition = leftTireTransform.getTransformationMatrix() * glm::vec4(0, y_offset, 0, 1);
 			if (glm::length(leftTirePosition - leftTireTracks.g_previousPosition()) > 1) {
@@ -190,17 +193,17 @@ void updateCarVFX(ecs::Scene mainScene, float dt) {
 		VFXParticleSystem& rightTireParticle = mainScene.GetComponent<VFXParticleSystem>(vfx.trackGuids[1]);
 		VFXParticleSystem& leftTireParticle = mainScene.GetComponent<VFXParticleSystem>(vfx.trackGuids[2]);
 
-		if (!previousStates[i][0])
+		if (!car->m_Vehicle.mBaseState.roadGeomStates[0].hitState && car->m_Vehicle.mBaseState.roadGeomStates[1].hitState)
 			frontTireParticle.active = false;
 		else
 			frontTireParticle.active = true;
 
-		if (!previousStates[i][1])
+		if (!car->m_Vehicle.mBaseState.roadGeomStates[2].hitState)
 			rightTireParticle.active = false;
 		else
 			rightTireParticle.active = true;
 
-		if (!previousStates[i][2])
+		if (!car->m_Vehicle.mBaseState.roadGeomStates[3].hitState)
 			leftTireParticle.active = false;
 		else
 			leftTireParticle.active = true;
