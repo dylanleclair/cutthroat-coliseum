2f31dc569d1d5c11b80dafa57d0676e502193527
dylanleclair <dylan.leclair@icloud.com>
2023-03-27 10:06:57 -0600

    revert lap ocunt

--- code/client/systems/RaceSystem.cpp
+++ code/client/systems/RaceSystem.cpp
@@ -67,7 +67,7 @@ void RaceTracker::Update(ecs::Scene& scene, float deltaTime) {
     {
       std::cout << "completed a lap!";
       car.lapCount++;
-      if (car.lapCount == 1)
+      if (car.lapCount == 3)
       {
         m_raceFinished = true;
       }
